b8cec961005572f2e571d45d3d0db06050d91aa7
umairrrkhan <umair64066@gmail.com>
2026-06-12 21:34:37 +0530

    wrote the simple method for for mobile low bandwidth clients

--- umni-backend/src/main/java/com/umni/repository/MessageRepository.java
+++ umni-backend/src/main/java/com/umni/repository/MessageRepository.java
@@ -3,10 +3,18 @@ package com.umni.repository;
 import com.umni.model.Message;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
+import java.time.Instant;
 
 public interface MessageRepository extends MongoRepository {
+	
+	interface MessagePreview{
+		String getId();
+		String getContent();
+		Instant getCreatedAt(); 
+	}
 	
 	List<Message> findByChatIdOrderByCreatedAsc(String chatId);
 	
 	void deletedByChatId(String chatId);
+	
 }
