e5907ea37e0de7d345d230a5a61a87d115df8a76
umairrrkhan <umair64066@gmail.com>
2026-06-14 16:37:03 +0530

    home page is now clean no trash data

--- umni-frontend/src/pages/homePage.jsx
+++ umni-frontend/src/pages/homePage.jsx
@@ -7,12 +7,8 @@ import '../css/HomePage.css';
 
 const Homepage = () => {
     const navigate = useNavigate();
+    const {chatId} = useParams();
     const [mode , setMode] = React.useState('text');
-    const[conversation , setConversation] = React.useState([
-        {id:1 , title:'Chat 1' , group:'yesterday'},
-        {id:2 , title:'Chat 2' , group:'lastWeek'},
-        {id:3 , title:'Chat 3' , group:'lastMonth'},
-    ]);
 
     const handleNewChat = () => {
         navigate('/home');
@@ -20,7 +16,7 @@ const Homepage = () => {
 
     return (
         <div className='home-layout'>
-            <Sidebar conversation={conversation} onNewChat={handleNewChat}/>
+            <Sidebar onNewChat={handleNewChat}/>
             <div className='main-content'>
                 <div className='mode-toggle'>
                     <button onClick={() => setMode('text')} className={`mode-btn ${mode === 'text' ? 'active' : ''}`}>Text Mode</button>
